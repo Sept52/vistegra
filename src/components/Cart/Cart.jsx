7c49f0d2d94c910d2c0239cc89f233d585a5e4c1
Danila Lavrenov <danila.sept@mail.ru>
2022-07-17 18:23:51 +0300

    changed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,11 +9,11 @@ function Cart() {
 
   const handleInput = () => {
     const interval = setInterval(() => {
-      const items = document.querySelectorAll(".cart__item");
+      const items = document.querySelectorAll('.cart__item');
       let totalPrice = 0;
       items?.forEach((item) => {
-        const priceEl = item.querySelector("[data-counter]");
-        totalPrice += parseInt(priceEl.innerText.replace(" руб.", ""));
+        const priceEl = item.querySelector('[data-counter]');
+        totalPrice += parseInt(priceEl.innerText.replace(' руб.', ''));
       });
       setCheck(totalPrice);
     });
@@ -21,40 +21,42 @@ function Cart() {
   };
 
   return (
-    <div>
+    <div className={s.cart}>
       <h2>Корзина</h2>
-      <table>
-        <colgroup>
-          <col style={{ width: "40%" }} />
-          <col style={{ width: "15%" }} />
-          <col style={{ width: "15%" }} />
-          <col style={{ width: "10%" }} />
-          <col style={{ width: "1%" }} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th scope="col">Наименование</th>
-            <th scope="col">Кол-во</th>
-            <th scope="col">Цена/шт</th>
-            <th scope="col">Сумма</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {itemsInCart.map((i) => {
-            return <CartItem item={i} key={i.ID} onChange={handleInput} />;
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Итого</td>
-            <td></td>
-            <td></td>
-            <td>{check} руб.</td>
-            <td></td>
-          </tr>
-        </tfoot>
-      </table>
+      <div className={s.cart__block}>
+        <table>
+          <colgroup>
+            <col style={{ width: '40%' }} />
+            <col style={{ width: '15%' }} />
+            <col style={{ width: '15%' }} />
+            <col style={{ width: '10%' }} />
+            <col style={{ width: '1%' }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th scope='col'>Наименование</th>
+              <th scope='col'>Кол-во</th>
+              <th scope='col'>Цена/шт</th>
+              <th scope='col'>Сумма</th>
+              <th scope='col'></th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemsInCart.map((i) => {
+              return <CartItem item={i} key={i.ID} onChange={handleInput} />;
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Итого</td>
+              <td></td>
+              <td></td>
+              <td>{check} руб.</td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 }
