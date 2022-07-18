9951a943128b704706883ea57b99017b5b7afdc2
Danila Lavrenov <danila.sept@mail.ru>
2022-07-18 17:26:17 +0300

    Updates

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { getInfoReducer } from './getInfoReducer';
-import { itemInCartReducer } from './itemInCartReducer';
-
-export const rootReducer = combineReducers({
-  getInfoReducer,
-  itemInCartReducer,
-});
