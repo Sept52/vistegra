aeb8b90e1aced807e19c9379bb69316598b04bcd
Danila Lavrenov <danila.sept@mail.ru>
2022-07-19 12:52:09 +0300

    del apply

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { rootReducer } from './redux/rootReducer';
 import { Provider } from 'react-redux';
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  )
+  
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
